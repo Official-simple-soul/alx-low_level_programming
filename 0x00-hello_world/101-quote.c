c37c0c05eb740e6a5688a75f8d9ec3d1b1bfcc8d
Official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-09-08 14:56:48 -0700

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
+
 #include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
  * Return:1
  */
-init main(void)
+int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; write(2, ch, sizeof(ch));
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, ch, sizeof(ch));
 	return (1);
-}
